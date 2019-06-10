d60d88bfb227d0e689228ce08977adcad0c17b16
UserGithub <kcqckj@163.com>
2019-06-10 19:44:27 +0800

    第一次提交代码

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $http = 'http://www.cxwt.xyz/php_32.zip';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="<?php echo $http ?>">下载</a>
+</body>
+</html>
